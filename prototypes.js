b887cc5f1c8372ea8167d383207cdc44fe1c4b7d
adam <bcuz@users.noreply.github.com>
2018-12-21 11:49:35 -0500

    game stretch

--- prototypes.js
+++ prototypes.js
@@ -99,58 +99,92 @@ Humanoid.prototype.greet = function() {
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+
+function Hero(attrs) {
+  Humanoid.call(this, attrs);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+Hero.prototype.attack = function(enemy) {
+  
+  if (Math.random() < .5) {
+    enemy.healthPoints -= 5
+    return enemy.takeDamage()
+  } else {
+    return `${this.name}'s attack missed!`    
+  } 
+}
+
+function Villain(attrs) {
+  Humanoid.call(this, attrs);
+}
+
+Villain.prototype = Object.create(Humanoid.prototype);
+
+Villain.prototype.attack = function(enemy) {
+  
+  if (Math.random() < .5) {
+    enemy.healthPoints -= 5
+    return enemy.takeDamage()
+  } else {
+    return `${this.name}'s attack missed!`    
+  } 
+}
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
-  // const mage = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 1,
-  //     height: 1,
-  //   },
-  //   healthPoints: 5,
-  //   name: 'Bruce',
-  //   team: 'Mage Guild',
-  //   weapons: [
-  //     'Staff of Shamalama',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const mage = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const swordsman = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 2,
-  //     height: 2,
-  //   },
-  //   healthPoints: 15,
-  //   name: 'Sir Mustachio',
-  //   team: 'The Round Table',
-  //   weapons: [
-  //     'Giant Sword',
-  //     'Shield',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const swordsman = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const archer = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 1,
-  //     width: 2,
-  //     height: 4,
-  //   },
-  //   healthPoints: 10,
-  //   name: 'Lilith',
-  //   team: 'Forest Kingdom',
-  //   weapons: [
-  //     'Bow',
-  //     'Dagger',
-  //   ],
-  //   language: 'Elvish',
-  // });
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+  
 
   // console.log(mage.createdAt); // Today's date
   // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
@@ -168,3 +202,34 @@ Humanoid.prototype.greet = function() {
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  // could make them fight until someone reaches 0.
+  // console.log(swordsman.healthPoints); // 15
+  // console.log(mage.attack(swordsman)); // Bruce took damage.  
+  // console.log(swordsman.healthPoints); // 15
+
+  function game() {
+    while (mage.healthPoints > 0 && swordsman.healthPoints > 0) {
+      if (Math.random() < .5) {
+        console.log(mage.attack(swordsman))
+      } else {
+        console.log(swordsman.attack(mage))
+      }
+  
+      if (mage.healthPoints <= 0) {
+        console.log(mage.destroy())
+      }
+      if (swordsman.healthPoints <= 0) {
+        console.log(swordsman.destroy())
+      }
+    }
+  }
+
+  game()
+
+  // // repeat game 5 times
+  // for (let i = 0; i < 5; i++) {
+  //   mage.healthPoints = 5
+  //   swordsman.healthPoints = 15
+  //   game()    
+  // }
